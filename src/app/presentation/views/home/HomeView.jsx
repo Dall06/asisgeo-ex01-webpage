1f05c59f5cc240b7c1bf7eee1231c18b64550399
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2021-09-07 18:18:27 -0500

    update: resources

--- src/app/presentation/views/home/HomeView.jsx
+++ src/app/presentation/views/home/HomeView.jsx
@@ -10,7 +10,7 @@ const HomeView = () => {
     const history = useHistory();
 
     return (
-        <Container className="Container">
+        <Container className="home-Container">
             <Row>
                 <img src="https://sig.esri.co/wp-content/uploads/2020/03/ES_0320_Banner_COVID-19_GL_01-1.png" alt="geo" />
                 <Container className="HeaderContainer">
@@ -21,6 +21,9 @@ const HomeView = () => {
                 </Container>
                 <Container className="CardContainer">
                     <Button variant="primary" className="Button" onClick={() => history.push("/asisgeo-ex01-webpage/intro")}>Saber mas</Button>
+                </Container>
+                <Container className="CardContainer" style={{marginTop: 62}}>
+                    <a className="btn Button btn-primary" href="https://img-prueba-delr.s3.us-east-2.amazonaws.com/asisgeo-ex01-report.pdf" download>Descargar</a>
                 </Container>
             </Row>
         </Container>
